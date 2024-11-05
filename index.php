f8b4dd1f40dc3192930deb877cb584a86bf69a5f
VARTOTOJAS <@>
2024-11-05 18:03:03 +0200

    lohas

--- index.php
+++ index.php
@@ -1,23 +1,42 @@
-<?php
-$servername="localhost";
-$username="username";
-$password="";
-$dbname="login";
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
 
-$conn = new mysqli($servername, $username, $dbname , $password);
+<main>
+    <nav>
+        <ul>
+            <li><a href="">Automobiliai</a></li>
 
-if ($conn->connect_errno) {
-    die("connection file: " . $conn->connect_error);
-}
+            <li><a href="">Foto</a></li>
 
-$sql = "INSERT INTO myguest(email, password)";
+            <li><a href="">Audio</a></li>
 
-if ($conn->qery(sql) === TRUE) {
-    echo "yra";
-}
-else {
-    echo "ERROR :"  . $sql . "<br>" . $conn->error;
-}
+            <li><a href="">Drabužiai</a></li>
 
-?>
+            <li><a href="">Avalinė</a></li>
 
+            <li><a href="">Technika</a></li>
+
+            <li><a href="">Baldai</a></li>
+
+            <li><a href="">Sodui</a></li>
+
+            <li><a href="">Žaidimams</a></li>
+
+            <li><a href="">Vaikams</a></li>
+
+            <li><a href="">Pasiūlimai</a></li>
+
+        </ul>
+    </nav>
+</main>
+
+
+</body>
+</html>
