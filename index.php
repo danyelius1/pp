ed72edc4257e8f58c86503dd4f080c78207a943a
VARTOTOJAS <@>
2024-11-12 17:45:41 +0200

    gfg

--- index.php
+++ index.php
@@ -5,37 +5,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="parallax.js"></script>
 </head>
 <body>
 
-<main>
-    <nav>
-        <ul>
-            <li style="background-color: #c87979;"><a href="" style=""><b>Automobiliai</b></a></li>
+<div class="parallax" data-parallax="scroll" data-image-src="image.jpg">
+    <main>
+        <nav>
+            <ul>
+                <li style="background-color: #bd3f3f;"><a href=""><b>Automobiliai</b></a></li>
 
-            <li style="background-color: #7ccc6e;"><a href=""><b>Foto</b></a></li>
+                <li style="background-color: #7ccc6e;"><a href=""><b>Foto</b></a></li>
 
-            <li style="background-color: #b965c3;"><a href=""><b>Audio</b></a></li>
+                <li style="background-color: #b965c3;"><a href=""><b>Audio</b></a></li>
 
-            <li style="background-color: #67d1be;"><a href=""><b>Drabužiai</b></a></li>
+                <li style="background-color: #67d1be;"><a href=""><b>Drabužiai</b></a></li>
 
-            <li style="background-color: #d6c863;"><a href=""><b>Avalinė</b></a></li>
+                <li style="background-color: #d6c863;"><a href=""><b>Avalinė</b></a></li>
 
-            <li style="background-color: #5c6bca;"><a href=""><b>Technika</b></a></li>
+                <li class="" style="background-color: #5c6bca;"><a href=""><b>Technika</b></a></li>
 
-            <li style="background-color: #da7759;"><a href=""><b>Baldai</b></a></li>
+                <li class="" style="background-color: #da7759;"><a href=""><b>Baldai</b></a></li>
 
-            <li style="background-color: #679c5f;"><a href=""><b>Sodui</b></a></li>
+                <li style="background-color: #679c5f;"><a href=""><b>Sodui</b></a></li>
 
-            <li style="background-color: #7e3434;"><a href=""><b>Žaidimams</b></a></li>
+                <li style="background-color: #7e3434;"><a href=""><b>Žaidimams</b></a></li>
 
-            <li style="background-color: #7a5e5e;"><a href=""><b>Vaikams</b></a></li>
+                <li style="background-color: #7a5e5e;"><a href=""><b>Vaikams</b></a></li>
 
-            <li style="background-color: #8a58da;"><a href=""><b>Pasiūlimai</b></a></li>
+                <li style="background-color: #8a58da;"><a href=""><b>Pasiūlimai</b></a></li>
 
-        </ul>
-    </nav>
-</main>
+            </ul>
+        </nav>
+    </main>
+</div>
+
 
 
 </body>
