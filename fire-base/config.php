66357d298db173ef8224a2e097acbebf74807dde
VARTOTOJAS <@>
2025-01-21 17:35:47 +0200

    hvgggy

---
+++ fire-base/config.php
@@ -0,0 +1,4 @@
+<?php
+
+session_start();
+$databaseURL = "https://user-ac3f4-default-rtdb.europe-west1.firebasedatabase.app/";
---
+++ fire-base/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<form>
+
+    <main>
+        <div class="email-input">
+            <label for="email"></label>
+            <input type="email" id="email" name="email" placeholder="gmail" required>
+        </div>
+
+        <div class="password-input">
+            <label for="password"></label>
+            <input type="password" id="password" name="password" placeholder="  password" required>
+        </div>
+
+        <div class="submit-bottom">
+            <input type="submit" id="submit" name="submit" value="Submit">
+        </div>
+    </main>
+
+</form>
+
+</body>
+</html>
